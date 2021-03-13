bd41db2c6300f8ccfcdf964dc6fb3d118ed783e5
K S M Musa Mohinuddin <musamohinuddin@gmail.com>
2021-03-13 21:31:27 +0530

    Added all the Assets, Packages, ProjectSettings fils

---
+++ Assets/Scripts/BackScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BackScript : MonoBehaviour
+{
+    public Button button;
+	public string BackScene;
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(OnButtonClick);	
+		
+    }	
+		
+	private void OnButtonClick()
+	{
+		Debug.Log("Button Clicked");
+		SceneManager.LoadScene(BackScene);
+	}	
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MyFirstScript : MonoBehaviour
+{
+    public Button button;	
+	public string newScene;
+	
+	
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(OnButtonClick);	
+		
+    }
+	
+	private void OnButtonClick()
+	{
+		Debug.Log("Button Clicked");
+		SceneManager.LoadScene(newScene);
+	}	
+	
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
